15d725a5622ca2b6d85db6ca90656d549d5f3ccd
YoannHumeau <yoann.hum@gmail.com>
2021-04-21 21:06:13 +0200

    Aliments Delete Repository Tests

--- Backend/FoodCounter.Api/FoodCounter.Tests/Api/Repositories/AlimentRepositoryTests.cs
+++ Backend/FoodCounter.Api/FoodCounter.Tests/Api/Repositories/AlimentRepositoryTests.cs
@@ -85,5 +85,58 @@ namespace FoodCounter.Tests.Api.Repositories
 
             result.Should().BeNull();
         }
+
+        [Fact]
+        public async void DeletedOneById_Ok()
+        {
+            PrepareDatabase();
+
+            int id = 2;
+
+            // Check the aliment exists
+            var resultBefore = await _alimentRepository.GetOneByIdAsync(id);
+            resultBefore.Should().BeEquivalentTo(AlimentDatas.listAliments.ElementAt(id - 1));
+
+            // Check that another aliment exists
+            var resultOtherBefore = await _alimentRepository.GetOneByIdAsync(id + 1);
+            resultOtherBefore.Should().BeEquivalentTo(AlimentDatas.listAliments.ElementAt(id));
+
+            // Delete the aliment
+            var result = await _alimentRepository.DeleteAsync(id);
+            result.Should().Be(true);
+
+            // Check the aliment is deleted
+            var resultAfter = await _alimentRepository.GetOneByIdAsync(id);
+            resultAfter.Should().BeNull();
+
+            // Check that the other aliment still exists
+            var resultOtherAfter = await _alimentRepository.GetOneByIdAsync(id + 1);
+            resultOtherAfter.Should().BeEquivalentTo(AlimentDatas.listAliments.ElementAt(id));
+        }
+
+        [Fact]
+        public async void DeletedOneById_Bad_NotFound()
+        {
+            PrepareDatabase();
+
+            int id = 777;
+            int otherId = 3;
+
+            // Check the aliment does not exists
+            var resultBefore = await _alimentRepository.GetOneByIdAsync(id);
+            resultBefore.Should().BeNull();
+
+            // Check that another aliment exists
+            var resultOtherBefore = await _alimentRepository.GetOneByIdAsync(otherId + 1);
+            resultOtherBefore.Should().BeEquivalentTo(AlimentDatas.listAliments.ElementAt(otherId));
+
+            // Try to delete 
+            var result = await _alimentRepository.DeleteAsync(id);
+            result.Should().Be(false);
+
+            // Check that the other aliment still exists
+            var resultOtherAfter = await _alimentRepository.GetOneByIdAsync(otherId + 1);
+            resultOtherAfter.Should().BeEquivalentTo(AlimentDatas.listAliments.ElementAt(otherId));
+        }
     }
 }
