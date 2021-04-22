1d2f9b4da6c07e238e77faebcbc159cb4b912265
YoannHumeau <yoann.hum@gmail.com>
2021-04-22 18:42:52 +0200

    Add Swagger

--- Backend/FoodCounter.Api/FoodCounter.Api/Startup.cs
+++ Backend/FoodCounter.Api/FoodCounter.Api/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace FoodCounter.Api
 {
@@ -32,6 +35,22 @@ namespace FoodCounter.Api
             services.AddScoped<IAlimentRepository, AlimentRepository>();
 
             services.AddScoped<DbAccess>(sp => new DbAccess(Configuration.GetConnectionString("Default")));
+
+            // Swagger configuration
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "FoodProject",
+                    Version = "v2",
+                    Description = "Food and calories counter application",
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +60,10 @@ namespace FoodCounter.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Swagger configuration
+            app.UseSwagger();
+            app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v2/swagger.json", "Food and calories counter application"));
 
             app.UseRouting();
 
