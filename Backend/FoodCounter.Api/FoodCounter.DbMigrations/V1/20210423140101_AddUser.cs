09ef35c91466b85f110f866e3a9a29ed56245f70
YoannHumeau <yoann.hum@gmail.com>
2021-04-25 02:38:31 +0200

    User Create Repository

---
+++ Backend/FoodCounter.Api/FoodCounter.DbMigrations/V1/20210423140101_AddUser.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+
+namespace FoodCounter.DbMigrations.V1
+{
+    /// <summary>
+    /// Migration user class
+    /// </summary>
+    [Migration(20210423140101)]
+    public class _20210423140101_AddUser : Migration
+    {
+        private static string _tableName = "Users";
+
+        public override void Down()
+        {
+            Delete.Table(_tableName);
+        }
+
+        public override void Up()
+        {
+            Create.Table(_tableName)
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Username").AsString(60).NotNullable()
+                .WithColumn("Password").AsString(256).NotNullable()
+                .WithColumn("Role").AsString(4).NotNullable();
+        }
+    }
+}
