d56e85524359265d687e267818450ec99d65010a
YoannHumeau <yoann.hum@gmail.com>
2021-04-28 12:27:38 +0200

    User GetOne Service

---
+++ Backend/FoodCounter.Api/FoodCounter.Api/Service/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using FoodCounter.Api.Entities;
+using System.Threading.Tasks;
+
+namespace FoodCounter.Api.Service
+{
+    /// <summary>
+    /// User service class
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Get one user by id
+        /// </summary>
+        /// <returns>One user</returns>
+        public Task<User> GetOneByIdAsync(long id);
+    }
+}
---
+++ Backend/FoodCounter.Api/FoodCounter.Api/Service/Implementations/UserService.cs
@@ -0,0 +1,31 @@
+﻿using FoodCounter.Api.Entities;
+using FoodCounter.Api.Repositories;
+using System.Threading.Tasks;
+
+namespace FoodCounter.Api.Service
+{
+    /// <summary>
+    /// User service class
+    /// </summary>
+    public class UserService : IUserService
+    {
+        private IUserRepository _userRepository;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="userRepository"></param>
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        ///<inheritdoc/>
+        public async Task<User> GetOneByIdAsync(long id)
+        {
+            var result = await _userRepository.GetOneByIdAsync(id);
+
+            return result;
+        }
+    }
+}
