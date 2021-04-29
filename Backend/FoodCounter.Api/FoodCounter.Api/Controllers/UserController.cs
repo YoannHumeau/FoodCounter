1d3e98cf5539fd4e2c8b29155eff875aa13e405d
YoannHumeau <yoann.hum@gmail.com>
2021-04-29 12:37:24 +0200

    User GetOne Controller

---
+++ Backend/FoodCounter.Api/FoodCounter.Api/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using FoodCounter.Api.Entities;
+using FoodCounter.Api.Models;
+using FoodCounter.Api.Resources;
+using FoodCounter.Api.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace FoodCounter.Api.Controllers
+{
+    /// <summary>
+    /// Users Controller
+    /// </summary>
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="mapper"></param>
+        /// <param name="userService"></param>
+        public UserController(ILogger<UserController> logger, IMapper mapper, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get one user by id
+        /// </summary>
+        /// <returns>One aliment</returns>
+        [HttpGet("{id}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetOneByIdAsync([FromRoute] long id)
+        {
+            var result = await _userService.GetOneByIdAsync(id);
+
+            if (result == null)
+                return NotFound(new { Message = ResourceEn.UserNotFound });
+
+            return Ok(result);
+        }
+    }
+}
