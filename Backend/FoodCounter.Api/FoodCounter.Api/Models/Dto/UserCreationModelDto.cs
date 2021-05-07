1441a4cada24b86521cd8de2126299821286f541
YoannHumeau <yoann.hum@gmail.com>
2021-05-07 13:41:33 +0200

    User Create Repository

---
+++ Backend/FoodCounter.Api/FoodCounter.Api/Models/Dto/UserCreationModelDto.cs
@@ -0,0 +1,18 @@
+﻿namespace FoodCounter.Api.Models.Dto
+{
+    /// <summary>
+    /// User creation class Dto
+    /// </summary>
+    public class UserCreationModelDto
+    {
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
