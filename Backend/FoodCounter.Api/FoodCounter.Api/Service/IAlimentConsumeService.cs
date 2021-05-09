47a15ddc87204a32fa1d30405e864218b2e921d9
YoannHumeau <yoann.hum@gmail.com>
2021-05-09 14:58:48 +0200

    Get AlimentConsume Service

---
+++ Backend/FoodCounter.Api/FoodCounter.Api/Service/IAlimentConsumeService.cs
@@ -0,0 +1,17 @@
+﻿using FoodCounter.Api.Models;
+using System.Threading.Tasks;
+
+namespace FoodCounter.Api.Service
+{
+    /// <summary>
+    /// AlimentConsume service class
+    /// </summary>
+    public interface IAlimentConsumeService
+    {
+        /// <summary>
+        /// Get one aliment consume by id
+        /// </summary>
+        /// <returns>One aliment</returns>
+        public Task<AlimentConsume> GetOneByIdAsync(long id);
+    }
+}
---
+++ Backend/FoodCounter.Api/FoodCounter.Api/Service/Implementations/AlimentConsumeService.cs
@@ -0,0 +1,31 @@
+﻿using FoodCounter.Api.Models;
+using FoodCounter.Api.Repositories;
+using System.Threading.Tasks;
+
+namespace FoodCounter.Api.Service.Implementations
+{
+    /// <summary>
+    /// AlimentConsume service class
+    /// </summary>
+    public class AlimentConsumeService : IAlimentConsumeService
+    {
+        private IAlimentConsumeRepository _alimentConsumeRepository;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="alimentConsumeRepository"></param>
+        public AlimentConsumeService(IAlimentConsumeRepository alimentConsumeRepository)
+        {
+            _alimentConsumeRepository = alimentConsumeRepository;
+        }
+
+        ///<inheritdoc/>
+        public async Task<AlimentConsume> GetOneByIdAsync(long id)
+        {
+            var result = await _alimentConsumeRepository.GetOneByIdAsync(id);
+
+            return result;
+        }
+    }
+}
