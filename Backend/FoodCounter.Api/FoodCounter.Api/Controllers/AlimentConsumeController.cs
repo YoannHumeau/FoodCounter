3563f6c5a8922b19d2c2d430ba6eb0372f421881
YoannHumeau <yoann.hum@gmail.com>
2021-05-09 22:02:21 +0200

    AlimentConsume GetAll for a user Controller

--- Backend/FoodCounter.Api/FoodCounter.Api/Controllers/AlimentConsumeController.cs
+++ Backend/FoodCounter.Api/FoodCounter.Api/Controllers/AlimentConsumeController.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Net.Mime;
 using System.Threading.Tasks;
+//using System.Security.Claims;
 
 namespace FoodCounter.Api.Controllers
 {
@@ -35,6 +37,33 @@ namespace FoodCounter.Api.Controllers
             _logger = logger;
             _alimentConsumeService = alimentConsumeService;
             _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get all the aliment consumes for a user (By default Himself) 
+        /// </summary>
+        /// <param name="userId">User Id to ask aliment consumes</param>
+        /// <returns>All aliments or one by search name</returns>
+        [HttpGet]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAsync([FromQuery] long userId)
+        {
+            if (userId == 0)
+            {
+                userId = Convert.ToInt64(User.Identity.Name);
+            }
+            else if (userId != Convert.ToInt64(User.Identity.Name) || !Helpers.IdentityHelper.IsUserAdmin(User))
+            {
+                return Forbid();
+            }
+
+            var result = await _alimentConsumeService.GetAllByUserIdAsync(userId);
+
+            var resultDto = _mapper.Map<IEnumerable<AlimentConsumeDto>>(result);
+
+            return Ok(result);
         }
 
         /// <summary>
