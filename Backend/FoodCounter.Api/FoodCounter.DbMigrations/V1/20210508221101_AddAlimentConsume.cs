debb32ea76fa8ec8146c46df30413382fad8fab0
YoannHumeau <yoann.hum@gmail.com>
2021-05-09 13:38:04 +0200

    Add AlimentConsume Migration

---
+++ Backend/FoodCounter.Api/FoodCounter.DbMigrations/V1/20210508221101_AddAlimentConsume.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+
+namespace FoodCounter.DbMigrations.V1
+{
+    /// <summary>
+    /// Migration aliment consumed class
+    /// </summary>
+    [Migration(20210508221101)]
+    public class _20210508221101_AddAlimentConsume : Migration
+    {
+        /// <summary>
+        /// Down migration
+        /// </summary>
+        public override void Down()
+        {
+            Delete.Table("Alimentconsumes");
+        }
+
+        /// <summary>
+        /// Up migration
+        /// </summary>
+        public override void Up()
+        {
+            Create.Table("Alimentconsumes")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("UserId").AsInt64().NotNullable()
+                .ForeignKey("Users", "Id")
+                .WithColumn("AlimentId").AsInt64().NotNullable()
+                .ForeignKey("Aliments", "Id")
+                .WithColumn("ConsumeDate").AsDateTime().NotNullable()
+                .WithColumn("Weight").AsInt32().WithDefaultValue(0)
+                ;
+        }
+    }
+}
