828e450f2fb75e9d70fade2d2aaccfd0cc3a096b
YoannHumeau <yoann.hum@gmail.com>
2021-05-10 12:51:11 +0200

    AlimentConsume Update Controller

---
+++ Backend/FoodCounter.Api/FoodCounter.Api/Models/Dto/AlimentConsumeUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodCounter.Api.Models.Dto
+{
+    /// <summary>
+    /// Aliment Consume Update Model Creation Dto
+    /// </summary>
+    public class AlimentConsumeUpdateDto
+    {
+        /// <summary>
+        /// Weight of the aliment consume
+        /// </summary>
+        [Required]
+        public int? Weight { get; set; }
+    }
+}
