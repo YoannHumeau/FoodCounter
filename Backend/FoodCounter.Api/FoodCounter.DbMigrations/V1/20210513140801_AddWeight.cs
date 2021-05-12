2f25f55f79bc97341ff38ad966f7fd70196b5917
YoannHumeau <yoann.hum@gmail.com>
2021-05-12 15:22:24 +0200

    Add UserWeight Migrations

---
+++ Backend/FoodCounter.Api/FoodCounter.DbMigrations/V1/20210513140801_AddWeight.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+
+namespace FoodCounter.DbMigrations.V1
+{
+    /// <summary>
+    /// Migration Weight class
+    /// </summary>
+    [Migration(20210513140801)]
+    public class _20210513140801_AddWeight : Migration
+    {
+        private readonly string _tableName = "UserWeights";
+
+        /// <summary>
+        /// Down migration
+        /// </summary>
+        public override void Down()
+        {
+            Delete.Table(_tableName);
+        }
+
+        /// <summary>
+        /// Up migration
+        /// </summary>
+        public override void Up()
+        {
+            Create.Table(_tableName)
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("UserId").AsInt64().NotNullable()
+                .WithColumn("WeightDate").AsDate().NotNullable()
+                .WithColumn("Weight").AsInt32().NotNullable()
+                ;
+        }
+    }
+}
