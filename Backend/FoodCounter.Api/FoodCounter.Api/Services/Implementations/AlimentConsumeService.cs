c6b9f89f43882b9375c165ad31499f57ceeadccc
Yoann Humeau <yoannhumdev@gmail.com>
2021-06-21 12:28:37 +0200

    Make a new GetOneById async to prevent double check user authorisation

--- Backend/FoodCounter.Api/FoodCounter.Api/Services/Implementations/AlimentConsumeService.cs
+++ Backend/FoodCounter.Api/FoodCounter.Api/Services/Implementations/AlimentConsumeService.cs
@@ -36,13 +36,25 @@ namespace FoodCounter.Api.Services.Implementations
             return result;
         }
 
-        ///<inheritdoc/>
-        public async Task<AlimentConsume> GetOneByIdAsync(long id)
+        /// <summary>
+        /// Get one aliment consume by id, method used by others to throw http exception if not exists
+        /// </summary>
+        /// <param name="id">aliment consume id</param>
+        /// <returns>One aliment consume</returns>
+        private async Task<AlimentConsume> GetOneByIdAsyncPrivate(long id)
         {
             var result = await _alimentConsumeRepository.GetOneByIdAsync(id);
 
             if (result == null)
                 throw new HttpNotFoundException(ResourceEn.AlimentConsumeNotFound);
+
+            return result;
+        }
+
+        ///<inheritdoc/>
+        public async Task<AlimentConsume> GetOneByIdAsync(long id)
+        {
+            var result = await GetOneByIdAsyncPrivate(id);
 
             if (result.UserId != Convert.ToInt64(_hcontext.User.Identity.Name) && !Helpers.IdentityHelper.IsUserAdmin(_hcontext.User))
                 throw new HttpForbiddenException(ResourceEn.AccessDenied);
@@ -65,7 +77,7 @@ namespace FoodCounter.Api.Services.Implementations
         public async Task<AlimentConsume> UpdateAsync(AlimentConsume newAlimentConsume)
         {
             // Check aliment-consume exists : Will throw http custom exception from GetOneByIdAsync() if does not exists
-            var aliment = await GetOneByIdAsync(newAlimentConsume.Id);
+            var aliment = await GetOneByIdAsyncPrivate(newAlimentConsume.Id);
 
             if(aliment.UserId != Convert.ToInt64(_hcontext.User.Identity.Name) && !Helpers.IdentityHelper.IsUserAdmin(_hcontext.User))
                 throw new HttpForbiddenException(ResourceEn.AccessDenied);
@@ -79,7 +91,7 @@ namespace FoodCounter.Api.Services.Implementations
         public async Task<bool> DeleteAsync(long id)
         {
             // Check aliment-consume exists : Will throw http custom exception from GetOneByIdAsync() if does not exists
-            var aliment = await GetOneByIdAsync(id);
+            var aliment = await GetOneByIdAsyncPrivate(id);
 
             if (aliment.UserId != Convert.ToInt64(_hcontext.User.Identity.Name) && !Helpers.IdentityHelper.IsUserAdmin(_hcontext.User))
                 throw new HttpForbiddenException(ResourceEn.AccessDenied);
